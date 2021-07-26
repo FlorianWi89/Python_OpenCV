f2ce228b75e6c60d99068b7e676820a806b01b01
Florianwi98 <florianwicher94@gmail.com>
2021-07-26 19:56:14 +0200

    drawed a rectangle

--- file1.py
+++ file1.py
@@ -6,9 +6,10 @@ import cv2
 image = cv2.imread("./picture_flo.jpg")
 
 img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-plt.imshow(img)
-plt.show()
+
 
 imgGrey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.rectangle(imgGrey, (1000, 5000), (2000, 4000), (240, 240, 240), 20)
 plt.imshow(imgGrey, "gray")
 plt.show()
